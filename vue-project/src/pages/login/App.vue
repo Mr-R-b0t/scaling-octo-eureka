71bf33636e2a213363882bd154968439db957e8f
Remi1906 <remi93300@hotmail.fr>
2023-03-01 15:48:24 -0500

    login page and his css

---
+++ vue-project/src/pages/login/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<header></header>
+
+	<main>
+		<div class="login">
+			<div class="top-login">
+				<h2>Sign in</h2>
+			</div>
+			<div class="body-login">
+				<form action="">
+					<input type="text" placeholder="Email id" class="input-login" />
+					<input type="text" placeholder="Password" class="input-login" />
+					<div class="options">
+						<a href="">forgot password?</a>
+						<a href="">create an account</a>
+					</div>
+					<a href="../register/index.html"
+						><input type="button" value="Login" class="input-login"
+					/></a>
+				</form>
+			</div>
+		</div>
+	</main>
+</template>
---
+++ vue-project/src/pages/register/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<header></header>
+
+	<main>
+		<div class="login">
+			<div class="top-login">
+				<h2>Register</h2>
+			</div>
+			<div class="body-register">
+				<form action="">
+					<input type="text" placeholder="Email id" class="input-register" />
+					<input type="text" placeholder="First name" class="input-register" />
+					<input type="text" placeholder="Last name" class="input-register" />
+					<input type="text" placeholder="Password" class="input-register" />
+					<input
+						type="text"
+						placeholder="Comfirm password"
+						class="input-register"
+					/>
+					<input type="button" value="Create" class="input-register" />
+				</form>
+			</div>
+		</div>
+	</main>
+</template>
