2e21f2b311a55017f664e0734ca6cc42665e23b1
Areesss13 <113906793+Areesss13@users.noreply.github.com>
2023-03-08 20:12:44 +0100

    added node modules

--- movielist/src/views/AboutView.vue
+++ movielist/src/views/AboutView.vue
@@ -1,4 +1,24 @@
 <template>
   <div class="about">
-  </div>
+    <h1>Popular Movie</h1>
+    <ul class="cadre">
+        <li class="cadre2">
+          <img alt="Vue logo" src="../assets/logo.png">
+        </li>
+        <li class="cadre2">
+          <img alt="Vue logo" src="../assets/logo.png">
+        </li>
+        <li class="cadre2">
+          <img alt="Vue logo" src="../assets/logo.png">
+        </li>
+
+        <li class="cadre2">
+          <img alt="Vue logo" src="../assets/logo.png">
+        </li>
+
+        <li class="cadre2">
+          <img alt="Vue logo" src="../assets/logo.png">
+        </li>
+      </ul>
+    </div>
 </template>
