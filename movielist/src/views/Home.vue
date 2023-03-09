809c54b5e56d93c69cf242faf0d8dd35fd482cf6
Areesss13 <113906793+Areesss13@users.noreply.github.com>
2023-03-09 21:06:44 +0100

    Create Home.vue

---
+++ movielist/src/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+    <body>
+    <header>
+        <div class="nom">
+          <h1>Popular Movies</h1>
+        </div>
+    </header>
+
+    <div style='display:flex'>
+
+      <div><img src='..\img\ouioui.jpg'></div>
+      
+      <div><img src='..\img\ouioui.jpg'></div>
+
+      <div><img src='..\img\ouioui.jpg'></div>
+      
+      <div><img src='..\img\ouioui.jpg'></div>
+      
+      <div><img src='..\img\ouioui.jpg'></div>
+      
+      </div>
+
+      <div style='display:flex'>
+
+        <div><img src='..\img\ouioui.jpg'></div>
+        
+        <div><img src='..\img\ouioui.jpg'></div>
+  
+        <div><img src='..\img\ouioui.jpg'></div>
+        
+        <div><img src='..\img\ouioui.jpg'></div>
+        
+        <div><img src='..\img\ouioui.jpg'></div>
+        
+        </div>
+
+        <div style='display:flex'>
+
+          <div><img class="site" src='..\img\netflix.jpg'></div>
+          
+          <div><img class="site" src='..\img\disney.jpg'></div>
+    
+          <div><img class="site" src='..\img\amazon.jpg'></div>
+          
+          </div>
+  </body>
+</template>
+<style>
+    h1 {
+    color: white;
+    }
+    body {
+    background-color: black;
+    margin-left : 400px ;
+    }
+    img {
+    width: 100px;
+    height: 150px;
+    margin : 30px;
+    border-radius : 10px;
+    }
+    .site {
+      width: 200px;
+    height: 100px;
+    margin : 30px;
+    border-radius : 10px;
+    }
+  </style>
