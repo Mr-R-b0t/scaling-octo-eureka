42b97d2e3d61519b121c3866a0ab3b70a7ab8ae9
Dorian <dorian.turgot@gmail.com>
2023-03-16 16:05:13 -0400

    added movie list page

--- movielist/src/router/index.js
+++ movielist/src/router/index.js
@@ -51,6 +51,15 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: 'about' */ '../views/FilmPageView.vue')
+  },
+  {
+    path: '/movielist',
+    name: 'movielist',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: 'about' */ '../views/MovieListView.vue')
   }
 ]
 
---
+++ movielist/src/views/MovieListView.vue
@@ -0,0 +1,43 @@
+<template>
+    <router-view />
+    <Menu />
+    <Friends />
+    <body>
+      <div id="home">
+        <h1>Movie lists</h1>
+        <div class="films" id="scrolling-wrapper">
+          <div class="card"><img src='../assets/img/avatar.jpg'>
+            <h4>Nom liste</h4>
+          </div>
+          <div class="card"><img src='../assets/img/antmanQ.jpg'>
+            <h4>Nom liste</h4>
+          </div>
+          <div class="card"><img src='../assets/img/chatpotte.jpg'>
+            <h4>Nom liste</h4>
+          </div>
+          <div class="card"><img src='../assets/img/asterixEmpire.jpg'>
+            <h4>Nom liste</h4>
+          </div>
+          <div class="card"><img src='../assets/img/creed3.jpg'>
+            <h4>Nom liste</h4>
+          </div>
+          <div class="card"><img src='../assets/img/topgun.jpg'>
+          </div>
+        </div>
+      </div>
+    </body>
+  </template>
+  <style>
+  @import '../assets/scss/home.scss'
+  </style>
+
+<script>
+import Menu from '../components/MenuView.vue'
+import Friends from '../components/FriendsView.vue'
+export default {
+  components: {
+    Menu,
+    Friends
+  }
+}
+</script>
