07038f5cc8f63e9ff0cba7fc2df328d376852c81
Sébastien <52678042+Mr-R-b0t@users.noreply.github.com>
2023-03-20 14:15:37 -0400

    Update db.config.js

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: 'box.toxicsed.fr',
     PORT: 49158,
     USER: 'root',
-    PASSWORD: '123Soleil',
+    PASSWORD: 'PASSWORD',
     DB : 'movie_list',
     dialect: "mysql",
 }
