893a0d3e3bea8c3baac6dbcdfb7e6386a69ea7c2
Dorian <dorian.turgot@gmail.com>
2023-03-20 20:01:56 -0400

    tmdb

--- movielist/src/views/HomeView.vue
+++ movielist/src/views/HomeView.vue
@@ -2,39 +2,12 @@
   <router-view />
   <Menu />
   <Friends />
+
   <body>
     <div id="home">
       <h1>Popular Movies</h1>
-      <div class="films" id="scrolling-wrapper">
-        <div class="card"><img src='../assets/img/avatar.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/antmanQ.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/chatpotte.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/asterixEmpire.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/creed3.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/topgun.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/babylon.jpg'>
-          <h4>Nom film</h4>
-        </div>
-         <div class="card"><img src='../assets/img/megan.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/scream6.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/tirailleurs.jpg'>
-          <h4>Nom film</h4>
+      <div id="movie-container">
+        <div class="films" id="scrolling-wrapper">
         </div>
       </div>
     </div>
@@ -42,16 +15,16 @@
     <h1>Upcoming Movies</h1>
     <div class="films">
       <div class="card"><img src='../assets/img/creed3.jpg'>
-        </div>
+      </div>
 
       <div class="card"><img src='../assets/img/megan.jpg'>
-        </div>
+      </div>
 
       <div class="card"><img src='../assets/img/asterixEmpire.jpg'>
       </div>
 
       <div class="card"><img src='../assets/img/tirailleurs.jpg'>
-        </div>
+      </div>
 
       <div class="card"><img src='../assets/img/buzz.jpg'>
       </div>
@@ -72,16 +45,16 @@
     <h1>Continue watching</h1>
     <div class="films">
       <div class="card"><img src='../assets/img/creed3.jpg'>
-        </div>
+      </div>
 
       <div class="card"><img src='../assets/img/megan.jpg'>
-        </div>
+      </div>
 
       <div class="card"><img src='../assets/img/asterixEmpire.jpg'>
       </div>
 
       <div class="card"><img src='../assets/img/tirailleurs.jpg'>
-        </div>
+      </div>
 
       <div class="card"><img src='../assets/img/buzz.jpg'>
       </div>
@@ -108,10 +81,14 @@
 <script>
 import Menu from '../components/MenuView.vue'
 import Friends from '../components/FriendsView.vue'
+import { displayPopularMovies } from '@/tmdbAPI'
 export default {
   components: {
     Menu,
     Friends
+  },
+  mounted() {
+    displayPopularMovies()
   }
 }
 </script>
