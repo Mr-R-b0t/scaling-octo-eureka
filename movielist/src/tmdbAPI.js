25d0f64d1e45f6a001e58d5a577e09c9727ded98
Remi1906 <remi93300@hotmail.fr>
2023-03-20 22:18:53 -0400

    mini update

--- movielist/src/tmdbAPI.js
+++ movielist/src/tmdbAPI.js
@@ -1,9 +1,5 @@
 const axios = require('axios')
 
-function callback(data) {
-  console.log(data)
-}
-
 function getPopularMovies() {
   return axios.get('https://api.themoviedb.org/3/movie/popular?api_key=66188e8432a52693bcb7abc97edbda86&language=en-US&page=1')
     .then(res => {
@@ -48,22 +44,20 @@ function getFilmPage(id) {
     .then(res => {
       const film = res.data
 
-      var name = film.title
-      var poster = 'https://image.tmdb.org/t/p/original' + film.poster_path
+      const name = film.title
+      const poster = 'https://image.tmdb.org/t/p/original' + film.poster_path
 
       const underTitle = film.runtime + 'mn - ' + film.release_date + ' - ' + film.production_countries[0].name
 
-      var underTitle = film.runtime + 'mn - ' + film.release_date + ' - ' + film.production_countries[0].name
+      const rating = film.vote_average + '/10 - ' + film.vote_count + ' votes'
 
-      var rating = film.vote_average + '/10 - ' + film.vote_count + ' votes'
+      const description = film.overview
 
-      var description = film.overview
-
-      var status = 'Status : ' + film.status
-      var genres = 'Genre : ' + film.genres[0].name
-      var budget = 'Budget : ' + film.budget + '$'
-      var revenue = 'Revenue : ' + film.revenue + '$'
-      var language = 'Language : ' + film.original_language
+      const status = 'Status : ' + film.status
+      const genres = 'Genre : ' + film.genres[0].name
+      const budget = 'Budget : ' + film.budget + '$'
+      const revenue = 'Revenue : ' + film.revenue + '$'
+      const language = 'Language : ' + film.original_language
 
       return [name, poster, underTitle, rating, description, status, genres, budget, revenue, language]
     })
@@ -76,7 +70,7 @@ function getFilmCard(id) {
   axios.get(' https://api.themoviedb.org/3/movie/' + id + '?api_key=66188e8432a52693bcb7abc97edbda86')
     .then(res => {
       const film = res.data
-      var poster = 'https://image.tmdb.org/t/p/original' + film.poster_path
+      const poster = 'https://image.tmdb.org/t/p/original' + film.poster_path
 
       return [film.name, poster]
     })
@@ -89,7 +83,7 @@ function getProvider(id) {
   axios.get(' https://api.themoviedb.org/3/movie/' + id + '/watch/providers?api_key=66188e8432a52693bcb7abc97edbda86')
     .then(res => {
       const film = res.data
-      var poster = 'https://image.tmdb.org/t/p/original' + film.results.CA.flatrate[0].logo_path
+      const poster = 'https://image.tmdb.org/t/p/original' + film.results.CA.flatrate[0].logo_path
       return [film.results.CA.flatrate[0].name, poster]
     })
     .catch(err => {
--- movielist/src/views/HomeView.vue
+++ movielist/src/views/HomeView.vue
@@ -6,100 +6,66 @@
   <body>
     <div id="home">
       <h1>Popular Movies</h1>
-      <div class="films" id="movie-container">
-      </div>
-      <h1>Popular Movies</h1>
-      <div class="films" id="scrolling-wrapper">
-        <div class="card"><img src='../assets/img/avatar.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/antmanQ.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/chatpotte.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/asterixEmpire.jpg'>
-          <h4>Nom film</h4>
-        </div>
+      <div class="films" id="movie-container"></div>
+
+      <h1>Upcoming Movies</h1>
+      <div class="films">
         <div class="card"><img src='../assets/img/creed3.jpg'>
-          <h4>Nom film</h4>
         </div>
-        <div class="card"><img src='../assets/img/topgun.jpg'>
-          <h4>Nom film</h4>
-        </div>
-        <div class="card"><img src='../assets/img/babylon.jpg'>
-          <h4>Nom film</h4>
-        </div>
+
         <div class="card"><img src='../assets/img/megan.jpg'>
-          <h4>Nom film</h4>
         </div>
-        <div class="card"><img src='../assets/img/scream6.jpg'>
-          <h4>Nom film</h4>
+
+        <div class="card"><img src='../assets/img/asterixEmpire.jpg'>
         </div>
+
         <div class="card"><img src='../assets/img/tirailleurs.jpg'>
-          <h4>Nom film</h4>
+        </div>
+
+        <div class="card"><img src='../assets/img/buzz.jpg'>
+        </div>
+
+        <div class="card"><img src='../assets/img/topgun.jpg'>
+        </div>
+
+        <div class="card"><img src='../assets/img/avatar.jpg'>
+        </div>
+
+        <div class="card"><img src='../assets/img/chatpotte.jpg'>
+        </div>
+
+        <div class="card"><img src='../assets/img/antmanQ.jpg'>
         </div>
       </div>
-    </div>
 
-    <h1>Upcoming Movies</h1>
-    <div class="films">
-      <div class="card"><img src='../assets/img/creed3.jpg'>
-      </div>
+      <h1>Continue watching</h1>
+      <div class="films">
+        <div class="card"><img src='../assets/img/creed3.jpg'>
+        </div>
 
-      <div class="card"><img src='../assets/img/megan.jpg'>
-      </div>
+        <div class="card"><img src='../assets/img/megan.jpg'>
+        </div>
 
-      <div class="card"><img src='../assets/img/asterixEmpire.jpg'>
-      </div>
+        <div class="card"><img src='../assets/img/asterixEmpire.jpg'>
+        </div>
 
-      <div class="card"><img src='../assets/img/tirailleurs.jpg'>
-      </div>
+        <div class="card"><img src='../assets/img/tirailleurs.jpg'>
+        </div>
 
-      <div class="card"><img src='../assets/img/buzz.jpg'>
-      </div>
+        <div class="card"><img src='../assets/img/buzz.jpg'>
+        </div>
 
-      <div class="card"><img src='../assets/img/topgun.jpg'>
-      </div>
+        <div class="card"><img src='../assets/img/topgun.jpg'>
+        </div>
 
-      <div class="card"><img src='../assets/img/avatar.jpg'>
-      </div>
+        <div class="card"><img src='../assets/img/avatar.jpg'>
+        </div>
 
-      <div class="card"><img src='../assets/img/chatpotte.jpg'>
-      </div>
+        <div class="card"><img src='../assets/img/chatpotte.jpg'>
+        </div>
 
-      <div class="card"><img src='../assets/img/antmanQ.jpg'>
-      </div>
-    </div>
-
-    <h1>Continue watching</h1>
-    <div class="films">
-      <div class="card"><img src='../assets/img/creed3.jpg'>
-      </div>
-
-      <div class="card"><img src='../assets/img/megan.jpg'>
-      </div>
-
-      <div class="card"><img src='../assets/img/asterixEmpire.jpg'>
-      </div>
-
-      <div class="card"><img src='../assets/img/tirailleurs.jpg'>
-      </div>
-
-      <div class="card"><img src='../assets/img/buzz.jpg'>
-      </div>
-
-      <div class="card"><img src='../assets/img/topgun.jpg'>
-      </div>
-
-      <div class="card"><img src='../assets/img/avatar.jpg'>
-      </div>
-
-      <div class="card"><img src='../assets/img/chatpotte.jpg'>
-      </div>
-
-      <div class="card"><img src='../assets/img/antmanQ.jpg'>
+        <div class="card"><img src='../assets/img/antmanQ.jpg'>
+        </div>
       </div>
     </div>
 
