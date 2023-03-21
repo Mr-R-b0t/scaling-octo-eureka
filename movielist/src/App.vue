514a33bbd476d21fbe24d5c5257a7af7565337e7
Dorian <dorian.turgot@gmail.com>
2023-03-20 22:16:24 -0400

    using discovery view to link to database

--- movielist/src/App.vue
+++ movielist/src/App.vue
@@ -1,3 +1,19 @@
 <template>
   <router-view />
 </template>
+<script>
+import MovieDataService from './services/MovieDataService'
+export default {
+  name: 'App',
+  mounted () {
+    MovieDataService.getAll()
+      .then(response => {
+        this.films = response.data
+        console.log(response.data)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+}
+</script>
